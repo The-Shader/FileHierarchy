072d10f0a35c93f6ca8690ad5a9c91bcc41836d2
David Tverdota <fireblade.sg@gmail.com>
2023-02-10 12:16:35 +0000

    Remove commented response

--- network/src/main/java/com/fireblade/network/user/GetCurrentUserResponse.kt
+++ network/src/main/java/com/fireblade/network/user/GetCurrentUserResponse.kt
@@ -3,10 +3,6 @@ package com.fireblade.network.user
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
-/*
-{"firstName":"Noel","lastName":"Flantier","rootItem":{"id":"4b8e41fd4a6a89712f15bbf102421b9338cfab11","parentId":"","name":"dossierTest","isDir":true,"modificationDate":"2021-11-29T10:57:13Z"}}
- */
-
 @Serializable
 data class GetCurrentUserResponse(
     @SerialName("firstName")
